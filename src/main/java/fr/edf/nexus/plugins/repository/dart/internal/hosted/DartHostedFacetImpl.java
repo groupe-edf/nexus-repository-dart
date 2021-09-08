62a434cbc1bf3c30f3b99f3375677b6a11303fcf
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2021-09-08 19:47:31 +0200

    add DartHostedFacet

---
+++ src/main/java/fr/edf/nexus/plugins/repository/dart/internal/hosted/DartHostedFacetImpl.java
@@ -0,0 +1,93 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2018-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package fr.edf.nexus.plugins.repository.dart.internal.hosted;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.sonatype.nexus.repository.storage.ComponentEntityAdapter.P_GROUP;
+import static org.sonatype.nexus.repository.storage.MetadataNodeEntityAdapter.P_NAME;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.sonatype.nexus.repository.FacetSupport;
+import org.sonatype.nexus.repository.storage.Query;
+import org.sonatype.nexus.repository.transaction.TransactionalStoreBlob;
+import org.sonatype.nexus.repository.transaction.TransactionalTouchMetadata;
+import org.sonatype.nexus.repository.view.Content;
+import org.sonatype.nexus.repository.view.Payload;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import fr.edf.nexus.plugins.repository.dart.internal.DartFacet;
+import fr.edf.nexus.plugins.repository.dart.internal.DartJsonProcessor;
+
+/**
+ * Default implementation of a Composer hosted facet.
+ */
+@Named
+public class DartHostedFacetImpl extends FacetSupport implements DartHostedFacet {
+
+    private final DartJsonProcessor dartJsonProcessor;
+
+    @Inject
+    public DartHostedFacetImpl(final DartJsonProcessor composerJsonProcessor) {
+        this.dartJsonProcessor = checkNotNull(composerJsonProcessor);
+    }
+
+    @Override
+    @TransactionalStoreBlob
+    public void upload(String name, String version, Payload payload) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    @TransactionalTouchMetadata
+    public Content getPackagesMetadatas() throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    @TransactionalTouchMetadata
+    public Content getPackageMetadatas(String name) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    @TransactionalTouchMetadata
+    public Content getPackageVersionMetadatas(String name, String version) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    @TransactionalStoreBlob
+    public Content getArchive(String path) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @VisibleForTesting
+    protected Query buildQuery(final String vendor, final String project) {
+        return Query.builder().where(P_GROUP).eq(vendor).and(P_NAME).eq(project).build();
+    }
+
+    private DartFacet content() {
+        return getRepository().facet(DartFacet.class);
+    }
+
+}
